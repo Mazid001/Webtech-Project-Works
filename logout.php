76ee5f63798a0afcfb7b942390d221e9d93322d1
Mazid001 <39764711+Mazid001@users.noreply.github.com>
2018-08-06 15:41:46 +0600

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	session_start();
-	session_destroy();
-	setcookie("id", "", time() - (86400 * 30), "/");
-	setcookie("type", "", time() - (86400 * 30), "/");
-	header("Location:login.php");
-
-?>
