7233b24c89da02650995b27a9613af2fba8b070b
Mazid001 <39764711+Mazid001@users.noreply.github.com>
2018-08-26 12:48:18 +0600

    updated

--- admin_sidebar.php
+++ admin_sidebar.php
@@ -2,26 +2,37 @@
 	if($_SESSION["type"]!="a")
 		header("Location:../login.php");
 ?>
-
+<head>
+    <style>
+    a:link {
+    text-decoration: none;
+   }
+   </style>
+</head>
 <td width="190" valign="top">
 			<b>&nbsp;Admin Panel</b><hr/>
+			
             <ul>
-                <li><a href="Admin_panel.php" target="iFrame">Dashboard</a></li>
-                <li><a href="students.php" target="iFrame">Students Info</a></li>
-                <li><a href="teachers.php" target="iFrame">Teachers Info</a></li>
-				<li><a href="searchuser.php" target="iFrame">Search User</a></li>
-                <li><a href="subject.php" target="iFrame">Add New Subject</a></li>
-				<li><a href="assignteacher.php" target="iFrame">Assign Teacher To Subject</a></li>
-				<li><a href="sendnotice.php" target="iFrame">Send Notice</a></li>
-				<li><a href="teacherattendance.php" target="iFrame">Teacher Attendance</a></li>
-				<li><a href="addadmin.php" target="iFrame">Add Admin</a></li>
-				<li><a href="teacherreport.php" target="iFrame">Generate Teacher Report</a></li>
-				<li><a href="studentreport.php" target="iFrame">Generate Student Report</a></li>
+                <li><a href="Admin_panel.php" >Dashboard</a></li>
+                <li><a href="students.php" >Students Info</a></li>
+                <li><a href="teachers.php" >Teachers Info</a></li>
+				<li><a href="searchuser.php" >Search User</a></li>
+                <li><a href="subject.php" >Add New Subject</a></li>
+				<li><a href="assignteacher.php" >Assign Teacher To Subject</a></li>
+				<li><a href="sendnotice.php" >Send Notice</a></li>
+				<li><a href="teacherattendance.php" >Teacher Attendance</a></li>
+				<li><a href="addadmin.php" >Add Admin</a></li>
+				<li><a href="teacherreport.php" >Generate Teacher Report</a></li>
+				<li><a href="studentreport.php" >Generate Student Report</a></li>
+				<li><a href="profile.php" >Profile</a></li>
+				<li><a href="uploadnotice.php" >Upload Notice</a></li>
             </ul> 
 			<b>&nbsp;Personal</b><hr/>
             <ul>
 			
-		    <li><a href="change_password.php" target="iFrame">Change Password</a></li>
+                <li><a href="change_password.php" >Change Password</a></li>
+                
             </ul>
+			
         </td>
         <td valign="top">
